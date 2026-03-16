98bf4c732610fdef66344d3a7eff530ea5d736d3
Muhammed Umer S <mohdumer77md@gmail.com>
2026-03-16 16:26:28 +0530

    Added Frontend and Backend Folder

---
+++ backend/src/main/java/com/example/placement_test_system/PlacementTestSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.placement_test_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacementTestSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacementTestSystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/placement_test_system/PlacementTestSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.placement_test_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacementTestSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
